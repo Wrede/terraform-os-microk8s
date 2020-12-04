647706c033fc528226d509ef5314fb8f5c56295f
Wrede <wrede.fredrik@gmail.com>
2020-12-04 15:14:44 +0100

    late commit

--- main.tf
+++ main.tf
@@ -1,38 +1,79 @@
 provider "openstack" {}
 
-data "user-data-controller" {
+resource "openstack_compute_keypair_v2" "keypair" {
+  name       = "${var.cluster_name}-keypair"
+  public_key = file(var.ssh_key_pub)
+}
+module "network" {
+  source              = "./modules/network"
+  name_prefix         = var.cluster_name
+  external_network_id = var.external_network_id
+}
+data template_file "userdata" {
+  template = file("${path.module}/templetes/userdata_microk8s.yml")
 
+  vars = {
+    microk8s_channel = "latest/stable" #var.microk8s_channel
+  }
 }
 
-module "security-groups" {
-  source = ""
-    
+module "ssh-sg" {
+  source               = "github.com/Wrede/terraform-os-security-group"
+  name                 = "ssh"
+  delete_default_rules = false
+  ingress_rules        = ["ssh-tcp"]
+  #ingress_with_cidr_blocks = [
+  #    {
+  #      from_port   = 25000
+  #      to_port     = 25000
+  #      protocol    = "tcp"
+  #      description = "microk8s master node"
+  #      cidr_blocks = "10.0.0.0/16"   #TODO: add subnet cidr output to network
+  #    },
+  #]
 }
 
-module "microk8s-cluster" {
-  source                       = "github.com/Wrede/terraform-os-instance/modules/master-slaves"
-  number_of_slaves             = 2
-  network_name                 = var.os_network_name
-  security_groups              = ["default"]
+module "controller" {
+  source                       = "github.com/Wrede/terraform-os-instance"
+  name                         = var.controller_name
+  number_of_instances          = 1
+  network_name                 = module.network.network_name
+  security_groups              = [module.ssh-sg.this_security_group_name[0]] #TODO: fix ouput in terraform-os-security-groups
   flavor_name                  = "ssc.small"
-  image_name                   = "CoreOS - latest"
-  assign_floating_ip_to_master = true
-  os_ssh_keypair               = var.os_keypair
-  name_master                  = "controller"
+  image_name                   = "Ubuntu 18.04" #TODO: create var
+  assign_floating_ip           = true
+  floating_ip_pool             = var.floating_ip_pool
+  os_ssh_keypair               = openstack_compute_keypair_v2.keypair.name
+  
+  user_data                    = data.template_file.userdata.rendered
 }
-    
+
+module "workers" {
+  source                       = "github.com/Wrede/terraform-os-instance"
+  name                         = var.prefix_name_workers
+  number_of_instances          = var.number_of_workers
+  network_name                 = module.network.network_name
+  security_groups              = [module.ssh-sg.this_security_group_name[0]]
+  flavor_name                  = "ssc.small"
+  image_name                   = "Ubuntu 18.04" #TODO: create var
+  assign_floating_ip           = false
+  os_ssh_keypair               = openstack_compute_keypair_v2.keypair.name
+  
+  user_data                    = data.template_file.userdata.rendered
 }
 
 resource "null_resource" "start-controller-node" {
-  count = "${var.clientnode_count}"
-
-  # trigger this resouce upon 'contoller' instance finishing 
-  triggers {
-    cluster_instance_ids = module.microk8s-cluster.module.master.this_instance_id
-  }
+  # trigger this resouce upon 'contoller' instance finishing
+  count      = var.start_k8s ? 1 : 0
+  depends_on = [
+      module.controller,
+    ]
+  #triggers = {
+  #  cluster_instance_ids = module.microk8s-cluster.master.this_instance_id
+  #}
 
   connection {
-    host = element(module.microk8s-cluster.module.master.this_instance_private_ip, 0)
+    host = element(module.controller.this_instance_private_ip, 0)
   }
   
   provisioner "remote-exec" {
@@ -44,20 +85,21 @@ resource "null_resource" "start-controller-node" {
 }
 
   resource "null_resource" "join_workers" {
+    count      = var.start_k8s ? var.number_of_workers : 0
+    
     depends_on = [
-      null_resource.start_controller_node,
+      null_resource.start-controller-node,
     ]
-    count = "${var.clientnode_count}"
-
+    
     #Run accross all worker nodes
     connection {
-      host = element(module.microk8s-cluster.module.slaves.this_instance_private_ip, count.index)
+      host = element(module.workers.this_instance_private_ip, count.index)
     }
     
     provisioner "remote-exec" {
       # Run service restart on each node in the clutser
       inline = [
-        "microk8s join ip-${module.microk8s-cluster.module.master.this_instance_private_ip[0]}:25000/${var.cluster_token}"
+        "microk8s join ${module.controller.this_instance_private_ip[0]}:25000/${var.cluster_token}"
       ]
     }
   }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,73 @@
-variable "microk8s_channel" {
+variable cluster_name {
+  type        = string
+  description = "Name of this cluster, used for naming network and keypair"
+  default     = "microk8s"  
+}
+
+variable microk8s_channel {
   type        = string
   description = "The installation channel (version) used for microk8s"
   default     = "latest/stable"   
 }
 
-variable "cluster_token" {
+variable cluster_token {
   type        = string
   description = "The token used for workers to join cluster"
-  default     = ""
+  default     = "i2CIwKagbV17T9gtFZUkMDYPc7IgniqgYsoMBKyRWYw="
 }
 
-variable "cluster_token_ttl" {
+variable cluster_token_ttl {
   type        = string
   description = "The token ttl used for workers to join cluster"
-  default     = ""
+  default     = "600"
 }
 
-variable "contoller_name" {
+variable start_k8s {
+  type        = bool
+  description = "If true, will start controller k8s node and workers join"
+  default     = false
+}
+
+variable number_of_workers {
+  type        = number
+  description = "Number of worker nodes to add to cluster"
+  default     = 2
+}
+
+variable controller_name {
   type        = string
   description = ""
   default     = "controller"
 }
 
-variable "prefix_name_workers" {
+variable prefix_name_workers {
   type        = string
   description = ""
   default     = "worker"
 }
 
-variable "os_keypair" {
+variable ssh_key_pub {
   type        = string
-  description = "Openstack ssh key that has already been created"
-  default     = ""
+  description = "Path to public key"
+  default = ""
+}
+
+#variable os_ssh_keypair {
+#  type        = string
+#  description = "Openstack ssh key that has already been created"
+#  default     = ""
+#}
+variable external_network_id {
+  type        = string
+  description = "The external network id in openstack"
+}
+
+#variable os_network_name {
+#  type        = string
+#  description = "An existing network in Openstack"
+#}
+
+variable floating_ip_pool {
+  type        = string
+  description = "Pool of floating IPs from which to assign to the controller"
 }
